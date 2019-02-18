29cbf53cacad5903990767a4ef0242da099135fe
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-02-18 15:41:46 +0000

    Missing path

--- Protection/tools/Protect.ps1
+++ Protection/tools/Protect.ps1
@@ -2,6 +2,6 @@ param ($ProjectPath = $(throw "Project path parameter is required."))
 		
 $searchString = ($ProjectPath + '\' + $BaseOutputPath)
 cd /D $ProjectPath
-& ($PSScriptRoot + "\DinkeyAddCmd.exe") ($ProjectPath + "Protection.dapfj") /a2
+& ($PSScriptRoot + "\DinkeyAddCmd.exe") ($ProjectPath + "\Protection.dapfj") /a2
 
 
