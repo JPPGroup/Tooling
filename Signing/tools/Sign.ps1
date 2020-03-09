4c0d5010d1ae4d2f3f750540839edf46c2706a7d
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-03-09 12:52:03 +0000

    Updated tooling to only sign assemblies when the CompanyName property includes JPP, and only on release builds. Protection package updated to reflect bump in signing dependency.

--- Signing/tools/Sign.ps1
+++ Signing/tools/Sign.ps1
@@ -5,7 +5,11 @@ $searchString = ($ProjectPath + '\' + $BaseOutputPath)
 $files = Get-ChildItem -Path $searchString -Include *.dll -Recurse
 
 foreach ($file in $files){	
-	& ($PSScriptRoot + "\signtool.exe") sign /sm /a /tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" $file		
+	$companyName = $file.VersionInfo.CompanyName
+	if($companyName.Contains("JPP"))
+    {
+		& ($PSScriptRoot + "\signtool.exe") sign /sm /a /tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" $file	
+	}		
 }
 
 
