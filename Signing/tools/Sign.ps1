d2e35922708dedef05b461df7bb61de6f9505290
MichaelLiddiard <michael.liddiard@jppuk.net>
2021-02-10 11:54:27 +0000

    Config does not have to match release, only contain to trigger signing

--- Signing/tools/Sign.ps1
+++ Signing/tools/Sign.ps1
@@ -1,5 +1,7 @@
 param ($BaseOutputPath = $(throw "Base Output Path parameter is required."), 				
 		$ProjectPath = $(throw "Project path parameter is required."))
+
+Write-Host "Searching for files to sign"
 		
 $searchString = ($ProjectPath + '\' + $BaseOutputPath)
 $files = Get-ChildItem -Path $searchString -Include *.dll -Recurse
@@ -8,7 +10,7 @@ foreach ($file in $files){
 	$companyName = $file.VersionInfo.CompanyName
 	if($companyName.Contains("JPP"))
     {
-		& ($PSScriptRoot + "\signtool.exe") sign /sm /a /tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" $file	
+		& ($PSScriptRoot + "\signtool.exe") sign /sm /a /tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" $file			
 	}		
 }
 
