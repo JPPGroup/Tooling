30fe4bb2f7cf1ae58cd1e23ae06b9c496e00fe7e
MichaelLiddiard <michael.liddiard@jppuk.net>
2021-04-07 16:28:21 +0100

    Added more debug messages to sign tool

--- Signing/tools/Sign.ps1
+++ Signing/tools/Sign.ps1
@@ -1,17 +1,21 @@
 param ($BaseOutputPath = $(throw "Base Output Path parameter is required."), 				
 		$ProjectPath = $(throw "Project path parameter is required."))
-
-Write-Host "Searching for files to sign"
 		
 $searchString = ($ProjectPath + '\' + $BaseOutputPath)
+Write-Host "Searching for files to sign in" $searchString
+
 $files = Get-ChildItem -Path $searchString -Include *.dll -Recurse
+$count = 0;
 
 foreach ($file in $files){	
 	$companyName = $file.VersionInfo.CompanyName
 	if($companyName.Contains("JPP"))
     {
 		& ($PSScriptRoot + "\signtool.exe") sign /sm /a /tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" $file			
-	}		
+		count++;
+	}			
 }
+
+Write-Host $files.Count "processed," $count "signed."
 
 
